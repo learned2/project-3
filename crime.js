978a0d02f15946043afda9f182d034e51a2d6be8
learned2 <susovicz@gmail.com>
2025-03-24 21:21:06 -0400

    Added time to popup

--- crime.js
+++ crime.js
@@ -47,6 +47,7 @@ let myMap = L.map("map", {
           let popupContent = `
             <strong>Offense Description:</strong> ${response[i].ofns_desc} <br>
             <strong>Date:</strong> ${response[i].cmplnt_fr_dt} <br>
+            <strong>Time:</strong> ${response[i].cmplnt_fr_tm} <br>
             <strong>Victim Age:</strong> ${response[i].vic_age_group} <br>
             <strong>Suspect Age:</strong> ${response[i].susp_age_group}
           `;
