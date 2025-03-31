74b61e7bf109050fd61cb3b013f9e963db7658ea
learned2 <susovicz@gmail.com>
2025-03-31 16:32:54 -0400

    Fixed charts, added all buttons

--- charts.js
+++ charts.js
@@ -3,24 +3,29 @@ let crimeBarChart;
 
 // Define colors for each crime type
 const crimeColors = {
-    "Murder": 'red',      // Murder: red color
-    "Arson": 'orange',    // Arson: orange color
-    "Larceny": 'green'    // Larceny: green color
+    "Murder": 'red',       
+    "Arson": 'orange',
+    "Auto Theft": 'blue',    
+    "Robbery": 'green'   
 };
 
 // Sample crime data
 const crimeData = {
     "Murder": {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        values: [12, 15, 18, 14, 20, 25, 10, 19, 13, 14, 17, 21]
+        values: [24, 19, 30, 20, 31, 32, 46, 21, 33, 34, 30, 23]
     },
     "Arson": {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        values: [5, 8, 7, 6, 12, 14, 10, 9, 13, 11, 6, 8]
+        values: [57, 56, 45, 57, 58, 38, 60, 45, 56, 53, 34, 31]
     },
-    "Larceny": {
+    "Auto Theft": {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        values: [45, 52, 48, 51, 60, 55, 70, 65, 59, 72, 68, 60]
+        values: [1140, 941, 1052, 1115, 1219, 1272, 1355, 1413, 1284, 1261, 1123, 844]
+    },
+    "Robbery": {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        values: [1423, 1248, 1295, 1342, 1473, 1446, 1511, 1491, 1369, 1477, 1307, 1129]
     }
 };
 
